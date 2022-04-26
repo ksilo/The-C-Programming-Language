da807f78907e04791fe69bae8181d6c8b24b9454
xilo <senobim@gmail.com>
2022-04-26 14:00:48 +0300

    initial commit

---
+++ ch5/5-7/getline.c
@@ -0,0 +1,14 @@
+/*
+ * Filename:    readlines.c
+ * Author:      xilo <xilo@rgl.lt>
+ * Date:        26-APR-2022
+ *
+ * The C Programming Language, second edition,
+ * by Brian Kernighan and Dennis Ritchie
+ *
+ * Exercise 5-7, page 110
+ *
+ * Rewrite readlines to store lines in an array suplied by main, rather than 
+ * calling alloc to maintain storage. How much faster is the program?
+ */
+

4332b8c8c694c807c615bdd48074dd387c1f4471
xilo <xilo@rgl.lt>
2022-04-26 13:45:07 +0300

    cleaning up

--- ch5/examples/sort/writelines.c
+++ ch5/examples/sort/writelines.c
@@ -3,6 +3,7 @@
 // writelines : write outputlines
 void writelines(char *lineptr[], int nlines)
 {
+	puts("--------------------------------");
 	while (nlines-- > 0)
 		printf("%s\n", *lineptr++);
 }

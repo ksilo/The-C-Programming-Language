88b5f433386ae55a0dbc2d41e172cee5017cac88
ksilo <senobim@gmail.com>
2022-04-29 08:40:42 +0300

    Update swap.c

--- ch5/5-7/swap.c
+++ ch5/5-7/swap.c
@@ -1,3 +1,17 @@
+/*
+ * Filename:    swap.c
+ * Author:      xilo <xilo@rgl.lt>
+ * Date:        26-APR-2022
+ *
+ * The C Programming Language, second edition,
+ * by Brian Kernighan and Dennis Ritchie
+ *
+ * Exercise 5-7, page 110
+ *
+ * Rewrite readlines to store lines in an array suplied by main, rather than 
+ * calling alloc to maintain storage. How much faster is the program?
+ */
+
 // swap: interchange v[i] and v[j]
 void swap(char *v[], int i, char j)
 {
